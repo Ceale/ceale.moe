bda434c1feff68206d9d21bc7a0483ee6ab945f4
筱莱Ceale <93030615+Ceale@users.noreply.github.com>
2026-05-18 04:25:15 +0800

    Add new link entry for '咻咔咻咔咻'
    Added a new entry for '咻咔咻咔咻' with its description, link, and icon.

--- src/assets/link.ts
+++ src/assets/link.ts
@@ -4,6 +4,12 @@ export const link = [
         desc: "数学生的古早拼贴风格网站",
         link: "https://xnmoe.com",
         icon: "https://www.xnmoe.com/assets/images/pfp.png"
+    },
+    {
+        name: "咻咔咻咔咻",
+        desc: "咻咔咻咔咻的个人记录",
+        link: "https://blog.shukashu.xyz/",
+        icon: "https://blog.shukashu.xyz/wp-content/uploads/2021/10/icon.png"
     }
 ] satisfies {
     name: string
